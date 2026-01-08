a0387de954d1b9a493c52bbe3bb27c180b434d3e
hwang-soo-yeon <hsy030226@snu.ac.kr>
2026-01-08 17:21:04 +0900

    FIX CODE
    최적 결과 연동되도록 plot_network 수정

--- model/config.py
+++ model/config.py
@@ -25,7 +25,7 @@ def create_config():
     config['data_file_path3'] = '../data/생관3-612-25-003_2025년 3월 일일생산계획 및 업체별 차종현황_Rev.00_25.02.28(확정).xlsx'
     # config['extra_data_file_path'] = '../data/02.의장·전기·기장 제작 계획 및 실적(24년 12월 예상실적).xlsm'
 
-    config['plot_network'] = False
+    config['plot_network'] = True
 
     # 결과 저장
     config['ymd'] = time.strftime('%Y%m%d')
